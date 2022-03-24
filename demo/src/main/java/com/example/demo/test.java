48a475463e854d8ba717a7100542e0e3b7f33356
74360 <743602584@qq.com>
2022-03-24 16:58:20 +0800

    第2

--- demo/src/main/java/com/example/demo/test.java
+++ demo/src/main/java/com/example/demo/test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @RequestMapping("/hello")
     public String handle01(){
-        return "Hello, Spring Bdasdsadadoot 2!22222";
+        return "Hello, Spring Bdasdsadadoot 2!222eqweq22";
     }
 
 }
